400bbbb4748c0b0b574fd828774414a443a8e69d
Jordan <jordanjosia929@gmail.com>
2025-03-15 23:06:52 +0800

    Update files.py

--- space shooter/code/files.py
+++ space shooter/code/files.py
@@ -1,16 +1,19 @@
 import pygame
 from os.path import join
 
-# Load the images
+#import 
 player_surf = pygame.image.load(join("5games-main", "space shooter", "images", "player.png")).convert_alpha()
-laser_surf = pygame.image.load(join("5games-main", "space shooter", "images", "laser.png")).convert_alpha()
+star_surf =  pygame.image.load(join("5games-main","space shooter","images", "star.png")).convert_alpha()
 meteor_surf = pygame.image.load(join("5games-main", "space shooter", "images", "meteor.png")).convert_alpha()
-star_surf = pygame.image.load(join("5games-main", "space shooter", "images", "star.png")).convert_alpha()
+laser_surf = pygame.image.load(join("5games-main", "space shooter", "images", "laser.png")).convert_alpha()
+explosion_frames = [pygame.image.load(join("5games-main", "space shooter", "images", "explosion", f"{i}.png")).convert_alpha() for i in range(21)]
+font = pygame.font.Font(join('5games-main', 'space shooter', 'images', 'Oxanium-Bold.ttf'), 20)
 
-# Create a dictionary to store the images
-images = {
-    "player": player_surf,
-    "laser": laser_surf,
-    "meteor": meteor_surf,
-    "star": star_surf
-}
+#sounds
+laser_sound = pygame.mixer.Sound(join("5games-main", "space shooter", "audio", "laser.wav"))
+damage_sound = pygame.mixer.Sound(join("5games-main", "space shooter", "audio", "damage.ogg"))
+game_music = pygame.mixer.Sound(join("5games-main", "space shooter", "audio", "game_music.wav"))
+explosion_sound = pygame.mixer.Sound(join("5games-main", "space shooter", "audio", "explosion.wav"))
+laser_sound.set_volume(0.5)
+game_music.set_volume(0.5)
+game_music.play(loops= 5)
