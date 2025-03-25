39131a30e7e1563fa18f9272acb31f064c86b08c
Jordan <jordanjosia929@gmail.com>
2025-03-25 22:54:03 +0800

    Create playerdummy.py

---
+++ Vampire survivor/code/playerdummy.py
@@ -0,0 +1,26 @@
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups, collision_sprite):
+        super().__init__(groups)
+        self.image = pygame.image.load(join("5games-main", "Vampire survivor", "images", "player", "down", "0.png")).convert_alpha()
+        self.rect = self.image.get_frect(center = pos)
+
+        #movement
+        self.direction = pygame.Vector2()
+        self.speed = 50
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+        self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+
+    def move(self, dt):
+        self.rect.x += self.direction.x * self.speed * dt
+        self.rect.y += self.direction.y * self.speed * dt
+
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
