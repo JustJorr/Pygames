d0e762be8e0053d09f78f9da9b877d290d2e21b0
Jordan <jordanjosia929@gmail.com>
2025-03-27 22:45:15 +0800

    Update playerdummy.py

--- Vampire survivor/code/playerdummy.py
+++ Vampire survivor/code/playerdummy.py
@@ -1,14 +1,18 @@
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, collision_sprite):
+    def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
+        self.load_images()
+        self.state, self.frames_index = "down", 0
         self.image = pygame.image.load(join("5games-main", "Vampire survivor", "images", "player", "down", "0.png")).convert_alpha()
         self.rect = self.image.get_frect(center = pos)
+        self.hitbox_rect = self.rect.inflate(0, 0)
 
         #movement
         self.direction = pygame.Vector2()
         self.speed = 50
+        self.collision_sprites = collision_sprites
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -17,10 +21,45 @@ class Player(pygame.sprite.Sprite):
         self.direction = self.direction.normalize() if self.direction else self.direction
 
     def move(self, dt):
-        self.rect.x += self.direction.x * self.speed * dt
-        self.rect.y += self.direction.y * self.speed * dt
+        self.hitbox_rect.x += self.direction.x * self.speed * dt
+        self.collision("horizontal")
+        self.hitbox_rect.y += self.direction.y * self.speed * dt
+        self.collision("vertical")
+        self.rect = self.hitbox_rect
 
+    def collision(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.hitbox_rect):
+                if direction == "horizontal":
+                    if self.direction.x > 0 : self.hitbox_rect.right = sprite.rect.left
+                    if self.direction.x < 0 : self.hitbox_rect.left = sprite.rect.right
+                else:
+                    if self.direction.y > 0 : self.hitbox_rect.bottom = sprite.rect.top
+                    if self.direction.y < 0 : self.hitbox_rect.top = sprite.rect.bottom
+
+    def load_images(self):
+        self.frames = {"down" : [], "up" : [], "right" : [], "left" : []}
+
+        for state in self.frames.keys():
+            for main_folder, sub_folder, file_names in walk(join("5games-main", "Vampire survivor", "images", "player", state)):
+                if file_names:
+                    for file_name in sorted(file_names, key= lambda name: int(name.split(".")[0])):
+                        full_path = join(main_folder, file_name)
+                        surf = pygame.image.load(full_path).convert_alpha()
+                        self.frames[state].append(surf)
+
+    def animate(self, dt):
+        #state
+        if self.direction.x != 0:
+            self.state = "right" if self.direction.x > 0 else "left"
+        if self.direction.y != 0:
+            self.state = "down" if self.direction.y > 0 else "up"
+
+        #animate
+        self.frames_index = self.frames_index + 5 * dt if self.direction else 0
+        self.image = self.frames[self.state][int(self.frames_index) % len(self.frames)]
 
     def update(self, dt):
         self.input()
         self.move(dt)
+        self.animate(dt)
