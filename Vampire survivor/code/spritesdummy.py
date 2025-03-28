9a1ae43f62fe95a4528c209aa4296e0cb0a844be
Jordan <jordanjosia929@gmail.com>
2025-03-28 23:07:19 +0800

    Update spritesdummy.py

--- Vampire survivor/code/spritesdummy.py
+++ Vampire survivor/code/spritesdummy.py
@@ -7,8 +7,8 @@ class Sprite(pygame.sprite.Sprite):
         self.rect = self.image.get_frect(topleft = pos)
 
 class CollisionSprite(pygame.sprite.Sprite):
-    def __init__(self, groups, pos, size):
+    def __init__(self, groups, pos, surf):
         super().__init__(groups)
-        self.image = pygame.Surface(size)
+        self.image = surf
         self.image.fill("blue")
         self.rect = self.image.get_frect(center = pos)
